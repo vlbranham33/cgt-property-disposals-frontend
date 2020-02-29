fef5ab7bdc032a6d5a203dfbed3291b86841fbdf
vlbranham33 <61508025+vlbranham33@users.noreply.github.com>
2020-02-29 13:38:27 -0700

    Revert "cpdp-1141: fix back links on the number of properties page (#220)"
    This reverts commit 48421f43e74e74d276079b50666a076e0a004b01.

--- app/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/returns/triage/InitialTriageQuestionsController.scala
+++ app/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/returns/triage/InitialTriageQuestionsController.scala
@@ -131,7 +131,7 @@ class InitialTriageQuestionsController @Inject() (
         Ok(
           howManyPropertiesPage(
             form,
-            howManyPropertiesBackLink(state),
+            routes.InitialTriageQuestionsController.whoIsIndividualRepresenting(),
             state.isRight
           )
         )
@@ -154,7 +154,7 @@ class InitialTriageQuestionsController @Inject() (
               BadRequest(
                 howManyPropertiesPage(
                   formWithErrors,
-                  howManyPropertiesBackLink(state),
+                  routes.InitialTriageQuestionsController.whoIsIndividualRepresenting(),
                   state.isRight
                 )
               ), { numberOfProperties =>
@@ -185,18 +185,6 @@ class InitialTriageQuestionsController @Inject() (
           )
     }
   }
-
-  private def howManyPropertiesBackLink(state: Either[StartingNewDraftReturn, FillingOutReturn]): Call =
-    triageAnswersFomState(state).fold(
-      _.fold(
-        _ => routes.InitialTriageQuestionsController.whoIsIndividualRepresenting(),
-        _ => routes.MultipleDisposalsTriageController.checkYourAnswers()
-      ),
-      _.fold(
-        _ => routes.InitialTriageQuestionsController.whoIsIndividualRepresenting(),
-        _ => routes.SingleDisposalsTriageController.checkYourAnswers()
-      )
-    )
 
   private def updateNumberOfProperties(
     state: Either[StartingNewDraftReturn, FillingOutReturn],
